de7ee6617e9899611e04b4ee3865ac1e8409895c
Pablo <apollo.zero@outlook.com>
2025-09-22 16:52:21 -0300

    feat: was made basic homepage

--- src/app/modules/global/pages/home/home.ts
+++ src/app/modules/global/pages/home/home.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Header } from '../../components/header/header';
 import { ButtonModule } from 'primeng/button';
 import { Sidebar } from '../../components/sidebar/sidebar';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Header, ButtonModule],
+  imports: [Header, ButtonModule, ToolbarModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
